ddd87a6e73b70610c1cfe223803bcd36eca95cfb
leehywo <leehywo@gmail.com>
2019-01-06 14:22:00 +0900

    Variable

---
+++ variable.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Varaible</h1>
+        <?php
+        $name = "egoing";
+        $b = 123;
+        echo "Lorem ipsum dolor ".$name." sit amet consectetur adipisicing".$name." elit. Fugiat, optio iste id exercitationem sint eveniet doloremque officia distinctio, fugit aliquam, ducimus qui quis odio ea! Doloribus vero quisquam sit nesciunt!"
+        ?>
+    </body>
+
+</html>
