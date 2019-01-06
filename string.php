6d00d9f901a998c652c5f91241c0027a942d6a44
leehywo <leehywo@gmail.com>
2019-01-06 14:11:42 +0900

    Number,String

---
+++ string.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <body>
+        <h1>String</h1>
+        <h2>String & String Operatoe</h2>
+        <?php
+        echo "Hello \"W\"orld!";
+        ?>
+        <h2>concatenation operator</h2>
+        <?php
+        echo "Hello"." World";
+        ?>
+        <h2>String length function</h2>
+        <?php
+        echo strlen("Hello"."world");
+        ?>
+    </body>
+</html>
