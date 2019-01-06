a3082f199ed4be847ac0d27533bc0462faf87976
leehywo <leehywo@gmail.com>
2019-01-06 19:31:29 +0900

    conditional

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <h2>
             <?php
             if (isset($_GET["id"])){
-            echo $_GET["id"];
+                echo $_GET["id"];
             }
             else {
                 echo ("Welcome!");
@@ -23,7 +23,7 @@
         </h2>
             <?php
             if(isset($_GET["id"])){
-            echo file_get_contents("data/".$_GET['id']);
+                echo file_get_contents("data/".$_GET['id']);
             } else{
                 echo ("Hello, PHP");
             }
