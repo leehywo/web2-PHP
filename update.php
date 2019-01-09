a9d08b2c0be550274b896c9f6e50d4abdb21ec32
leehywo <leehywo@gmail.com>
2019-01-09 21:26:13 +0900

    update

--- update.php
+++ update.php
@@ -53,5 +53,16 @@ function print_list(){
             <?php
             print_description();
             ?>
+        <form action = "update_process.php" method ="post">
+            <input type="hidden" name="old_title" value="<?=$_GET['id'];?>">
+            <p>
+                <input type="text" name="title" placeholder="title" value="<?php print_title();?>">
+            </p>
+            <p>
+                <textarea name="description" placeholder="description"><?php print_description();?></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
     </body>
 </html>
